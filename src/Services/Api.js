b5c0677a891e3f9ca7abc6ec336e5826b96d9c42
keys8410 <yanalmeidagarcia@gmail.com>
2020-11-05 12:36:34 -0300

    Camada de serviços configurada

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseUrl: 'http://localhost:3001/v1',
+  baseURL: 'http://localhost:3001/v1',
 });
 
 api.interceptors.request.use(async (config) => {
