d8e2712365ae1c62d8417a776356a716848c9a79
keys8410 <yanalmeidagarcia@gmail.com>
2020-11-04 22:27:59 -0300

    Url base + interceptors Axios configurados

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: `http://localhost:3001/v1/`,
+const api = axios.create({
+  baseUrl: 'http://localhost:3001/v1',
 });
+
+api.interceptors.request.use(async (config) => {
+  const token = window.localStorage.getItem('tkn');
+
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
+});
+
+export default api;
